09d4d4e874bd772ffc414725b1bf766ffb399dc2
Hazel Alain <hazelo235@gmail.com>
2023-11-27 23:37:11 -0600

    Initial Commit

---
+++ tablas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tablas
+//
+//  Created by Hazel Alain on 27/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
